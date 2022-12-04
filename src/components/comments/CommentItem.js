f9cd6745d82b80b46ae3b35f33dc28c01279b1d4
Rushikesh <60548902+RushikeshGandhmal@users.noreply.github.com>
2022-12-04 23:35:41 +0530

    Update CommentItem.js

--- src/components/comments/CommentItem.js
+++ src/components/comments/CommentItem.js
@@ -2,7 +2,7 @@ import classes from './CommentItem.module.css';
 
 const CommentItem = (props) => {
   return (
-    <li className={classes.item}>
+    <li className={classes. item}>
       <p>{props.text}</p>
     </li>
   );
